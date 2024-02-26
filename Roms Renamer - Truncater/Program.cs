5a7bb6d2da63d2a4cbc84398b0e0435f7c172af6
Washington Meneses <washington@wsxapps.com.br>
2024-02-26 16:35:09 -0300

    First Commit

---
+++ Roms Renamer - Truncater/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+string dir = @"O:\Games\Roms\ps2";
+
+var files = Directory.GetFiles(dir);
+
+foreach (var file in files)
+{
+    string newFile = file
+       .Replace(".gz", ".gz")
+    .Replace(" .gz", ".gz")
+    .Replace(".gz", ".gz");
+
+    try
+    {
+        System.IO.File.Move(file, newFile);
+
+    }
+    catch (Exception ex)
+    {
+
+    }
+
+}
